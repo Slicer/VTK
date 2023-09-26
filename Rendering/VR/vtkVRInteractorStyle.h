df940d5d2061150ad9289871a453fa86146e67ee
Mohamed Mssaouri <mohamed.mssaouri@kitware.com>
2023-09-26 14:04:47 +0200

    [Backport] Move teleportation to interaction modes
    Merge-request: vtk/vtk!10546
    (cherry picked from commit kitware/VTK@8e8b0fef743812e0343c3e88e89c529a4e286e48)
    (cherry picked from commit a923a456ae2e10747b11a1b108c4bfe98c6d069c)

--- Rendering/VR/vtkVRInteractorStyle.h
+++ Rendering/VR/vtkVRInteractorStyle.h
@@ -60,7 +60,6 @@ public:
   void OnMove3D(vtkEventData* edata) override;
   void OnMenu3D(vtkEventData* edata) override;
   void OnElevation3D(vtkEventData* edata) override;
-  void OnTeleportation3D(vtkEventData* edata) override;
   ///@}
 
   ///@{
@@ -109,6 +108,11 @@ public:
    * position (up/down).
    */
   void Elevation3D(vtkEventDataDevice3D*);
+
+  /**
+   * Instant movement to the picked point
+   */
+  void Teleportation3D(vtkEventDataDevice3D* edd);
 
   ///@{
   /**
