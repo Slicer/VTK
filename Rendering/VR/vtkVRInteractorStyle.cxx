d8dd3154902bf331c05c6cfb8b08963461bdf0b8
Alexy Pellegrini <alexy.pellegrini@kitware.com>
2023-11-30 09:56:21 +0100

    [Backport] VR: Add VTKIS_USCALE support in VR interactor style
    Merge-request: vtk/vtk!10735
    (cherry picked from commit kitware/VTK@d1699eb07f7972dd367979830b8c31536dd73180)
    (cherry picked from commit c003544e12be53c5f1a2484c4f977e45b2a552d0)

--- Rendering/VR/vtkVRInteractorStyle.cxx
+++ Rendering/VR/vtkVRInteractorStyle.cxx
@@ -312,6 +312,11 @@ void vtkVRInteractorStyle::OnMove3D(vtkEventData* edata)
       this->Clip(edd);
       this->InvokeEvent(vtkCommand::InteractionEvent, nullptr);
       break;
+    case VTKIS_USCALE:
+      this->FindPokedRenderer(x, y);
+      this->UniformScale();
+      this->InvokeEvent(vtkCommand::InteractionEvent, nullptr);
+      break;
     default:
       vtkDebugMacro(<< "OnMove3D: unknown interaction state " << idev << ": "
                     << this->InteractionState[idev]);
@@ -1020,6 +1025,9 @@ void vtkVRInteractorStyle::EndAction(int state, vtkEventDataDevice3D* edata)
       break;
     case VTKIS_TELEPORTATION:
       this->Teleportation3D(edata);
+      break;
+    case VTKIS_USCALE:
+      this->EndUniformScale();
       break;
     default:
       vtkDebugMacro(<< "EndAction: unknown state " << state);
